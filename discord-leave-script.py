7812ed9a80e9706c1d439ac34f3514d787b94a6b
Rico101Playz <106526171+Rico101Playz@users.noreply.github.com>
2022-06-27 18:36:27 +0200

    Updated method

--- discord-leave-script.py
+++ discord-leave-script.py
@@ -16,15 +16,15 @@ for i in range(0, cycles):
     time.sleep(0.3)
     screen = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0, 0, 2560, 1380))), cv2.COLOR_BGR2RGB)
     screen_gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-    result = cv2.matchTemplate(screen_gray, groupicogray, cv2.TM_SQDIFF)
+    result = cv2.matchTemplate(screen_gray, groupicogray, cv2.TM_SQDIFF_NORMED)
     mn,_,mnLoc,_ = cv2.minMaxLoc(result)
     xpos,ypos = mnLoc
     pag.click((xpos + 100), (ypos + 22))
     pag.click((xpos + 250), (ypos + 22))
-    time.sleep(0.1)
+    time.sleep(0.2)
     screen = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0, 0, 2560, 1380))), cv2.COLOR_BGR2RGB)
     screen_gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-    result = cv2.matchTemplate(screen_gray, leavegroupgray, cv2.TM_SQDIFF)
+    result = cv2.matchTemplate(screen_gray, leavegroupgray, cv2.TM_SQDIFF_NORMED)
     mn,_,mnLoc,_ = cv2.minMaxLoc(result)
     xpos,ypos = mnLoc
     pag.click((xpos+100), (ypos+20))
